7c3224546815df435451650f527a4ef980fc4b9a
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-10-29 01:18:34 +0300

    refactor: remove checkuser_api.sh from root (moved to src/)

--- checkuser_api.sh
+++
@@ -1,8 +0,0 @@
-api="https://checkuser.org"
-user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
-function check_username() {
-		curl --request POST \
-		--url "$api/availability_checker.php" \
-		--user-agent "$user_agent" \
-		--data "username=$1"
-}
